dec85f6cd053a3895906fcef2296d2b568dd9950
arcrofts2000 <arcrofts2000@gmail.com>
2024-02-28 18:53:27 +0000

    Added Base Project

---
+++ Source/CostumeParty/CostumeParty.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CostumeParty.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CostumeParty, "CostumeParty" );
---
+++ Source/CostumeParty/CostumePartyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CostumePartyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COSTUMEPARTY_API ACostumePartyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
